75ef49517f5b60eaf7f221c8d7651108ea1fc8c7
GregorJosh <grzegorz.jozw@gmail.com>
2023-10-11 19:20:11 +0200

    init

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,13 +2,15 @@ import { Button, Container } from "components";
 import styles from "./Hero.module.scss";
 
 export const Hero = () => {
-  const { section, container } = styles;
+  const { section, container, title, description } = styles;
 
   return (
     <section className={section}>
       <Container className={container}>
-        <h1>We Create Startups.</h1>
-        <p>We are startup studio that develops and launches new companies.</p>
+        <h1 className={title}>We Create Startups.</h1>
+        <p className={description}>
+          We are startup studio that develops and launches new companies.
+        </p>
         <Button label="See our works" />
       </Container>
     </section>
