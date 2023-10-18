1c8e7bd15d54d98cca94d2ab09eac0740a063ae7
GregorJosh <grzegorz.jozw@gmail.com>
2023-10-18 12:35:30 +0200

    init

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,72 @@
 import { Container } from "components";
-//import styles from "./Footer.module.scss";
+import styles from "./Footer.module.scss";
+import sprite from "images/sprite.svg";
 
 export const Footer = () => {
-  //const { logo } = styles;
+  const { container, title, links, link, address, logo, image, icons, icon } =
+    styles;
 
   return (
     <footer>
-      <Container>
-        <div>Logo</div>
-        <div>Companies</div>
-        <div>Contact</div>
-        <div>Follow us</div>
+      <Container className={container}>
+        <div className={logo}>
+          <svg className={image}>
+            <use href={sprite + "#logo"} />
+          </svg>
+          <p>© 2020 Startupz. All rights reserved. </p>
+        </div>
+        <div>
+          <h4 className={title}>Companies</h4>
+          <ul className={links}>
+            <li>
+              <a className={link} href="#tolq">
+                Tolq
+              </a>
+            </li>
+            <li>
+              <a className={link} href="#legalsite">
+                LegalSite
+              </a>
+            </li>
+            <li>
+              <a className={link} href="#codekeeper">
+                Codekeeper
+              </a>
+            </li>
+            <li>
+              <a className={link} href="#feedback-labs">
+                Feedback Labs
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className={title}>Contact</h4>
+          <address className={address}>
+            World Trade Center - The Hague Prinses Margrietplantsoen 33
+            <br />
+            2595 AM The Hague
+            <br />
+            The Netherlands
+          </address>
+          <a className={link} href="#email">
+            Send us an email
+          </a>
+        </div>
+        <div>
+          <h4 className={title}>Follow us</h4>
+          <div className={icons}>
+            <svg className={icon}>
+              <use href={sprite + "#twitter"} />
+            </svg>
+            <svg className={icon}>
+              <use href={sprite + "#linkedin"} />
+            </svg>
+            <svg className={icon}>
+              <use href={sprite + "#instagram"} />
+            </svg>
+          </div>
+        </div>
       </Container>
     </footer>
   );
